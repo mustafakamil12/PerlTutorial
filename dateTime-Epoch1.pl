bbab4b70c78719b3e74682bd4be2751a1cce0dcd
Mustafa Al Ogaidi <mustafaalogaidi@Mustafas-MacBook-Air.local>
2021-02-19 17:21:26 -0600

    add date time scripts

---
+++ dateTime-Epoch1.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/perl
+ 
+$epoc = time();
+
+print "Number of seconds since Jan 1, 1970 - $epoc\n";
---
+++ dateTime-Epoch2.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+$datestring = localtime();
+print "Current date and time $datestring\n";
+
+$epoc = time();
+$epoc = $epoc - 24 * 60 * 60;   # one day before of current date.
+
+$datestring = localtime($epoc);
+print "Yesterday's date and time $datestring\n";
