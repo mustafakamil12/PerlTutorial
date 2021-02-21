b26c68209525c1cad33132597656bfa962a4bd4b
Mustafa Al Ogaidi <mustafaalogaidi@Mustafas-MacBook-Air.local>
2021-02-21 14:58:09 -0600

    add IO scripts

---
+++ IO-Delete.pl
@@ -0,0 +1,3 @@
+#!/usr/bin/perl
+
+unlink ("/Users/mustafaalogaidi/Desktop/MyWork/TutorialsPoint-Perl/file2.txt");
---
+++ IO-Rename.pl
@@ -0,0 +1,3 @@
+#!/usr/bin/perl
+
+rename ("/Users/mustafaalogaidi/Desktop/MyWork/TutorialsPoint-Perl/file2.txt", "/Users/mustafaalogaidi/Desktop/MyWork/TutorialsPoint-Perl/file3.txt");
