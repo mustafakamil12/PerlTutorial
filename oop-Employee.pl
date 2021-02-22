090b3e38b725d6039df29c26a9f1cfcf93c7085d
Mustafa Al Ogaidi <mustafaalogaidi@Mustafas-MacBook-Air.local>
2021-02-22 14:05:39 -0600

    add oop scripts

---
+++ oop-Employee.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use Person;
+
+#use Person '/Users/mustafaalogaidi/Desktop/MyWork/TutorialsPoint-Perl/';
+
+$object = new Person( "Mohammad", "Saleem", 23234345);
+# Get first name which is set using constructor.
+$firstName = $object->getFirstName();
+
+print "Before Setting First Name is : $firstName\n";
+
+# Now Set first name using helper function.
+$object->setFirstName( "Mohd." );
+
+# Now get first name set by helper function.
+$firstName = $object->getFirstName();
+print "Before Setting First Name is : $firstName\n";
