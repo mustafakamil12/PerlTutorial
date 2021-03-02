a4996d3ee7520ec162d7206398ade2cd10dd557a
Mustafa Al Ogaidi <mustafaalogaidi@Mustafas-MacBook-Air.local>
2021-03-01 19:44:08 -0600

    update scalar scripts

---
+++ scalar_Operations.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+$str = "hello" . "world";       # Concatenates strings.
+$num = 5 + 10;                  # adds two numbers.
+$mul = 4 * 5;                   # multiplies two numbers.
+$mix = $str . $num;             # concatenates string and number.
+
+print "str = $str\n";
+print "num = $num\n";
+print "mul = $mul\n";
+print "mix = $mix\n";
