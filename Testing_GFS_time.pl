063e96c2656ba27e5414eb29ecaffa3779536bbe
Mustafa Al Ogaidi <mustafaalogaidi@Mustafas-MacBook-Air.local>
2021-08-21 02:32:45 -0500

    add more testing scripts

--- Testing_GFS_time.pl
+++ Testing_GFS_time.pl
@@ -1,7 +1,24 @@
 #!/usr/bin/perl
+use Data::Dumper;
+use lib "/Users/mustafaalogaidi/Desktop/MyWork/TutorialsPoint-Perl";     # Add library directory to lib path.
+use strict;
+use GFS_time;
 
-use GFS_test
+# Time/Date Strings
+my $today = new GFS_time;
+print Dumper($today);
+my $yyyy = $today->as_text("%Y");
+my $mm = $today->as_text("%m");
+my $dd = $today->as_text("%d");
+my $yyyymmdd = $yyyy . $mm . $dd;
+my $inputdate = "$yyyy-$mm-$dd";
 
-$format_time = new GFS_time;
+my $validtime = new GFS_time("$inputdate 00:00:00");
+print Dumper($validtime);
+my $initstring = $today->as_text("%Y") .
+	    "-" . $today->as_text("%m") .
+	    "-" .$today->as_text("%d") .
+	    " " . $today->as_text("%H:%M:%S");
 
-print "$format_time \n";
+my $param_code = 19;
+my $fcst_source = 3;
